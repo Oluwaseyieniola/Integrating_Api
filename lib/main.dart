f728a031f95c4aabd2b98dd2920cb8acd039ffab
Your Name <you@example.com>
2022-09-07 20:41:24 +0100

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:integrating_api/src/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: ' integrating API',
+        home: HomeScreen());
+  }
+}
